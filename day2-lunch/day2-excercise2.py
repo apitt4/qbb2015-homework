a268b3a0b21182b550b3769f95feb97359f4f38d
Ashley Pitt <apitt4@jh.edu>
2015-08-25 13:08:22 -0400

    Excercise 2 Day 2

---
+++ day2-lunch/day2-excercise2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+filename = "/Users/cmdb/qbb2015/genomes/Part3.sam"
+
+#How to open a file
+f = open ( filename )
+
+line_count = 0
+for lines in (f):
+    if "NM:i:0" in lines:
+        line_count +=1
+    else:
+        pass
+print line_count
